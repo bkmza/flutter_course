5fddeb3f7853ad21115d14010a6a7610b0081fc1
IlyaUsikov <IlyaUsikov@coherentsolutions.com>
2019-01-25 17:49:10 +0300

    added paddings

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -37,6 +37,24 @@ class ProductPage extends StatelessWidget {
   //       });
   // }
 
+  Widget _buildAddressPriceRow() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text('Union Square, San Francisco',
+            style: TextStyle(fontFamily: 'Oswald', color: Colors.grey)),
+        Container(
+            margin: EdgeInsets.symmetric(horizontal: 5.0),
+            child: Text('|',
+                style: TextStyle(
+                  color: Colors.grey,
+                ))),
+        Text('\$' + price.toString(),
+            style: TextStyle(fontFamily: 'Oswald', color: Colors.grey))
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
@@ -57,23 +75,7 @@ class ProductPage extends StatelessWidget {
               Image.asset(imageUrl),
               Container(
                   padding: EdgeInsets.all(10.0), child: TitleDefault(title)),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text('Union Square, San Francisco',
-                      style:
-                          TextStyle(fontFamily: 'Oswald', color: Colors.grey)),
-                  Container(
-                      margin: EdgeInsets.symmetric(horizontal: 5.0),
-                      child: Text('|',
-                          style: TextStyle(
-                            color: Colors.grey,
-                          ))),
-                  Text('\$' + price.toString(),
-                      style:
-                          TextStyle(fontFamily: 'Oswald', color: Colors.grey))
-                ],
-              ),
+              _buildAddressPriceRow(),
               Container(
                 padding: EdgeInsets.all(10.0),
                 child: Text(
